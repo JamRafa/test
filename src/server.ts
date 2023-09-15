2300475b0ea79d72fbc90d44f6e63e844168c3ff
JamRafa <rafa.deandrade35@gmail.com>
2023-09-15 14:48:37 -0300

    json

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,33 @@
 import express from 'express'
-import { prismaClient } from './database'
 
 const app = express()
 app.use(express.json())
 
 const port = process.env.PORT ?? 4000
 
-app.get('/books', async (request, response) => {
-  const books = await prismaClient.book.findMany()
-  return response.json(books)
-})
 
-app.post('/books', async (request, response) => {
-  const { description, name } = request.body
-  const book = await prismaClient.book.create({
-    data: {
-      description,
-      name,
-    },
-  })
-  return response.json(book)
-})
+// Rota GET para listar inscritos
+app.get("/inscritos", (req, res) => {
+  // Supondo que você tenha um arquivo "db.json" com os dados
+  const inscritos = require("./db.json").inscritos;
+  res.json(inscritos);
+});
 
-app.listen(port, () => console.log('Server is running on port ', port))
+// Rota POST para criar um novo inscrito
+app.post("/inscritos", (req, res) => {
+  const newInscrito = req.body;
+
+  // Supondo que você tenha um arquivo "db.json" com os dados
+  const db = require("./db.json");
+  db.inscritos.push(newInscrito);
+
+  // Salvar a atualização no arquivo "db.json"
+  const fs = require("fs");
+  fs.writeFileSync("./db.json", JSON.stringify(db, null, 2));
+
+  res.json(newInscrito);
+});
+
+app.listen(port, () => {
+  console.log(`Servidor está rodando na porta ${port}`);
+});
