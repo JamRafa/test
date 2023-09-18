797a88f316c3e69792e98afab0d531967eb4c0f1
JamRafa <rafa.deandrade35@gmail.com>
2023-09-18 16:20:57 -0300

    corsFinal

--- src/server.ts
+++ src/server.ts
@@ -21,7 +21,7 @@ const db = getDatabase(fireBaseApp);
 const app = express();
 app.use(express.json());
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "*");
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
   next();
