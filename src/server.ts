78cafadbb375acc9a3f0524cb484edec56166803
JamRafa <rafa.deandrade35@gmail.com>
2023-09-18 14:56:04 -0300

    cors

--- src/server.ts
+++ src/server.ts
@@ -20,7 +20,14 @@ const db = getDatabase(fireBaseApp);
 
 const app = express();
 app.use(express.json());
-app.use(cors({origin: "*"}))
+app.use((req, res, next) => {
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});
 
 const port = process.env.PORT ?? 4000;
 
